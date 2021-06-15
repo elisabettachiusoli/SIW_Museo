5e9601925c8fc83bea3dd42c9bf9740fc7ea115e
Elisabetta Chiusoli <47059153+elisabettachiusoli@users.noreply.github.com>
2021-06-16 01:56:09 +0200

    Update MuseoApplication.java

--- src/main/java/it/uniroma3/siw/MuseoApplication.java
+++ src/main/java/it/uniroma3/siw/MuseoApplication.java
@@ -2,12 +2,13 @@ package it.uniroma3.siw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class MuseoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MuseoApplication.class, args);
+	    public static void main(String[] args) {
+	        SpringApplication.run(MuseoApplication.class, args);
+	    }
 	}
-
-}
